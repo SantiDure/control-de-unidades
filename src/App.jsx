baafc967d6fa2c8a6fa0d84b3e7f55ca4861231c
SantiDure <santidure2002@gmail.com>
2024-08-02 08:01:39 -0300

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
 import './App.css'
+import Home from "./pages/home/Home";
 
 function App() {
- 
+ return  <>
+       <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+            </Route>
+          </Routes>
+      </BrowserRouter>
+ </>
 }
 
 export default App
